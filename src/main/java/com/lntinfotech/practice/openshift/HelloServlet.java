40b36f42845206ccc21e2e448b7599b7f60df129
Yogesh Jadhav <yogesh.jadhav@gmail.com>
2018-03-26 14:03:44 +0530

    Add files via upload
    Database call added.

--- src/main/java/com/lntinfotech/practice/openshift/HelloServlet.java
+++ src/main/java/com/lntinfotech/practice/openshift/HelloServlet.java
@@ -2,6 +2,11 @@ package com.lntinfotech.practice.openshift;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +29,10 @@ public class HelloServlet extends HttpServlet {
 
 		PrintWriter out = resp.getWriter();
 
+		resp.setContentType("text/html");
+
+		printDatabaseInformation(out);
+
 		printEnvVar(out);
 
 		printConfigMapVar(out);
@@ -34,22 +43,52 @@ public class HelloServlet extends HttpServlet {
 		out.close();
 	}
 
-	/**
-	 * @param out
-	 */
-	private void printConfigFromFile(PrintWriter out) {
-		String gameProperty = System.getenv("game_property");
-		if (gameProperty != null) {
-			out.println("Game Property : " + gameProperty);
-		} else {
-			out.println("Sorry, config map from property cannot be read.");
+	private void printDatabaseInformation(PrintWriter out) {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("mysql://mysql:3306/", "os_mysql", "os_mysql");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("show databases");
+			out.println("<pre>");
+			out.println("Database list");
+			while (rs.next()) {
+				out.println(rs.getString(0));
+			}
+			out.println("</pre>");
+			rs.close();
+		} catch (Exception e) {
+			out.println("Error while connecting databse: " + e.getMessage());
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					out.println("Error while closing connection." + e.getMessage());
+				}
+			}
 		}
 	}
 
 	/**
 	 * @param out
 	 */
+	private void printConfigFromFile(PrintWriter out) {
+		out.println("<pre>");
+		String gameProperty = System.getenv("game_property");
+		if (gameProperty != null) {
+			out.println("Game Property : " + gameProperty);
+		} else {
+			out.println("Sorry, config map from property cannot be read.");
+		}
+		out.println("</pre>");
+	}
+
+	/**
+	 * @param out
+	 */
 	private void printConfigMapVar(PrintWriter out) {
+		out.println("<pre>");
 		String propertyOne = System.getenv("example_property_1");
 		String propertyTwo = System.getenv("example_property_2");
 
@@ -59,6 +98,7 @@ public class HelloServlet extends HttpServlet {
 		} else {
 			out.println("Sorry, no config map found.");
 		}
+		out.println("</pre>");
 	}
 
 	/**
@@ -68,6 +108,7 @@ public class HelloServlet extends HttpServlet {
 	 * @throws IOException
 	 */
 	private void printEnvVar(PrintWriter out) throws IOException {
+		out.println("<pre>");
 		String username = System.getenv("username");
 		String password = System.getenv("password");
 		if ((username != null) && (password != null)) {
@@ -76,6 +117,7 @@ public class HelloServlet extends HttpServlet {
 		} else {
 			out.println("Sorry, no secret found.");
 		}
+		out.println("</pre>");
 	}
 
 }
